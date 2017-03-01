ec23254ecf54bd2a87067e3fcc3be6861afc61d8
yuanyu.lin <yuanyu.lin@gmail.com>
2017-03-01 14:18:01 +0800

    update unionfs_build script

--- unionfs_build.sh
+++ unionfs_build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-UPPER_DIR="./buildsystem/buildroot_top=RW"
+UPPER_DIR="./buildsystem/buildroot_top"
 BASE_DIR="./buildsystem/buildroot-2015.11.1"
 WORK_DIR="./buildsystem/buildroot"
 
 LOWER_DIR="./buildroot_default"
 
-unionfs-fuse -o nonempty $UPPER_DIR:$LOWER_DIR:$BASE_DIR $WORK_DIR
+mkdir -p $UPPER_DIR
+mkdir -p $WORK_DIR
+
+UPPER_DIR_RW="$UPPER_DIR=RW"
+
+unionfs-fuse -o nonempty $UPPER_DIR_RW:$LOWER_DIR:$BASE_DIR $WORK_DIR
 
